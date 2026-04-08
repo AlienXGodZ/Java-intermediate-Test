7749f80d198eaa7bf103d4a858dda77064a9c743
AlienXGodZ <allenharvey2005@gmail.com>
2026-04-09 01:07:53 +0530

    Test10.java

---
+++ Test10.java
@@ -0,0 +1,9 @@
+import mypackage.mypack;
+
+public class Test10 {
+    public static void main(String[] args) {
+
+        mypack s = new mypack(); // object creation
+        s.display();               // using method
+    }
+}
---
+++ mypackage/mypack.java
@@ -0,0 +1,11 @@
+package mypackage;
+
+public class mypack {
+    String name = "Allen";
+    int age = 20;
+
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
