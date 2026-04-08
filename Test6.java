b26a5fd38c0b307e267eaae9f7e48ccb4efcfa2f
AlienXGodZ <allenharvey2005@gmail.com>
2026-04-08 17:28:22 +0530

    Test6 added

---
+++ Test6.java
@@ -0,0 +1,14 @@
+//Method without return type
+public class Test6 {
+
+    // Method without return type
+    public static void addNumbers(int a, int b) {
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+
+    public static void main(String[] args) {
+
+        addNumbers(10, 20); // calling method
+    }
+}
