bd7f9111a447d52abb7ecd5ca3f1070c0902bf48
AlienXGodZ <allenharvey2005@gmail.com>
2026-04-09 12:40:14 +0530

    Test11 added

---
+++ Test11.java
@@ -0,0 +1,19 @@
+//Single inheritance (Animal → Dog)
+
+class Animal {
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog barks");
+    }
+
+    public static void main(String[] args){
+        Dog d = new Dog();
+        d.sound();  // parent method
+        d.bark();   // child method
+    }
+}
