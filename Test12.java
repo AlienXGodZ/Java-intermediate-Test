abd84d17aca53a4478a0d0d55b22780be89b68d8
AlienXGodZ <allenharvey2005@gmail.com>
2026-04-09 12:57:33 +0530

    Test12 added

---
+++ Test12.java
@@ -0,0 +1,27 @@
+//Multilevel inheritance (Animal → Dog → Puppy)
+
+class Animal1 {
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+class Dog1 extends Animal1 {
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Puppy extends Dog1 {
+    void weep() {
+        System.out.println("Puppy weeps");
+    }
+
+    public static void main(String[] args) {
+        Puppy p = new Puppy();
+
+        p.sound(); // from Animal
+        p.bark();  // from Dog
+        p.weep();  // from Puppy
+    }
+}
