f9d947035d18ff6e91bf0b97568e09e95a959b61
AlienXGodZ <allenharvey2005@gmail.com>
2026-04-11 13:09:39 +0530

    Test23 added

---
+++ Test23.java
@@ -0,0 +1,26 @@
+//Multiple interface implementation
+
+interface Camera {
+    void click();
+}
+
+interface Music {
+    void play();
+}
+
+class Phone implements Camera, Music {
+
+    public void click() {
+        System.out.println("Photo clicked");
+    }
+
+    public void play() {
+        System.out.println("Music playing");
+    }
+
+    public static void main(String[] args) {
+        Phone p = new Phone();
+        p.click();
+        p.play();
+    }
+}
