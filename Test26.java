5e53237bcdbb17516bb7ec60c75427ed96578356
AlienXGodZ <allenharvey2005@gmail.com>
2026-04-11 16:39:26 +0530

    Test26 added

---
+++ Test26.java
@@ -0,0 +1,21 @@
+//Member Inner Class
+
+class Outer {
+    String msg = "Hello from Outer";
+
+    class Inner {
+        void show() {
+            System.out.println(msg);
+        }
+    }
+
+    public class Test26 {
+
+
+        public static void main(String[] args) {
+            Outer o = new Outer();
+            Outer.Inner i = o.new Inner(); // important syntax
+            i.show();
+        }
+    }
+}
