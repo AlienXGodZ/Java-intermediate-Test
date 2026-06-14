9351ce4583dc29d84dd9537bf100b20a4c31b0f9
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-15 00:18:13 +0530

    Test34 added

---
+++ Test34.java
@@ -0,0 +1,15 @@
+import java.time.LocalTime;
+
+public class Test34{
+    public static void main(String[] args){
+
+        LocalTime mytime = LocalTime.now();
+
+        System.out.println(mytime);
+
+
+
+    }
+
+
+}
