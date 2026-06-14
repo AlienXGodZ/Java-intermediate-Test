1afddc1cf4c4fb464a7a7e9105e011b54f9c890d
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-15 00:13:48 +0530

    Test33 added

---
+++ Test33.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+public class Test33 {
+    public static void main(String[] args){
+
+        LocalDate myobj = LocalDate.now();
+        System.out.println(myobj);
+
+
+
+    }
+}
