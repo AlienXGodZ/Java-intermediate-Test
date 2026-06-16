7a2aa06855d8eb5f056049a87f843462faec1333
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-16 21:38:37 +0530

    Test49 added

--- Test49.java
+++ Test49.java
@@ -2,8 +2,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Test48 {
+public class Test49 {
     public static void main(String[] args) {
+
+        int count = -1;
 
         try {
             BufferedReader br =
@@ -11,11 +13,13 @@ public class Test48 {
 
             String line;
 
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
+            while (br.readLine() != null) {
+                count++;
             }
 
             br.close();
+
+            System.out.println("Lines: " + count);
 
         } catch (IOException e) {
             System.out.println("Error");
