26c49b4bb0d13710c3543aba7f6e262e9e95de1a
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-16 22:27:33 +0530

    Test52 added

---
+++ Test52.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Test52 {
+    public static void main(String[] args) {
+
+        try {
+            BufferedReader br =
+                    new BufferedReader(new FileReader("C:\\Users\\allen\\IdeaProjects\\filename.txt"));
+
+            String line;
+
+            while ((line = br.readLine()) != null) {
+
+                String[] data = line.split(" ");
+
+                int id = Integer.parseInt(data[0]);
+                String name = data[1];
+
+                System.out.println("ID: " + id);
+                System.out.println("Name: " + name);
+                break;
+            }
+
+            br.close();
+
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+    }
+}
