13144dfca67e90d615aa136ad2ecd4d69c12bb98
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-16 20:12:29 +0530

    Test46 added

--- Test46.java
+++ Test46.java
@@ -4,16 +4,11 @@ public class Test46 {
     public static void main(String[] args) {
 
         File myobj = new File("C:\\Users\\allen\\IdeaProjects\\filename.txt");
-        try {
-            if (myobj.delete()) {
-                System.out.println("delete the file " + myobj.getName());
-            } else {
-                System.out.println("Faiiled to delelte the file");
-            }
 
-        }catch(Exception e){
-            System.out.println("not deleted");
+        if (myobj.delete()) {
+            System.out.println("Deleted the file: " + myobj.getName());
+        } else {
+            System.out.println("Failed to delete the file");
         }
-
     }
 }
