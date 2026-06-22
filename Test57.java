35f5b1b5443cce8a5ee6779c0950b35e0b22b24d
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-22 14:52:20 +0530

    Test57 added

--- Test57.java
+++ Test57.java
@@ -1,2 +1,19 @@
+
 public class Test57 {
+
+    static void roll(int count, String result) {
+
+        if (count == 0) {
+            System.out.println(result);
+            return;
+        }
+
+        for (int i = 1; i <= 6; i++) {
+            roll(count - 1, result + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        roll(2, "");
+    }
 }
