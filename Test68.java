f78b59c93a64b291529a83fee865a9eaec8d37ad
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-24 18:36:44 +0530

    Test68 added

---
+++ Test68.java
@@ -0,0 +1,16 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Test68 {
+    public static void main(String[] args) {
+        // true = append mode
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("filename.txt", true))) {
+            bw.newLine();                      // move to a new line
+            bw.write("Appended line");         // add new text at the end
+            System.out.println("Successfully appended to the file.");
+        } catch (IOException e) {
+            System.out.println("Error writing file.");
+        }
+    }
+}
